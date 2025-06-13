082837c241bfe3e33ba89138fe38b6445fb5a05b
denchikut <ollylut@gmail.com>
2025-06-13 12:46:55 -0700

    feat: update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
+#!/usr/bin/env bash
+
 # Install brew if not installed
 command -v brew >/dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Brew installation
-brew install starship fzf eza bat zoxide tmux neovim stow
+brew install starship fzf eza bat zoxide tmux ripgrep fd neovim stow
 
 # Install oh-my-zsh
 [ -d "$HOME/.oh-my-zsh" ] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install oh-my-zsh plugins
-plugins="$HOME/.oh-my-zsh/custom/plugins"
-typeset -A plugins=(
-  "https://github.com/zsh-users/zsh-autosuggestions.git" "$plugins/zsh-autosuggestions"
-  "https://github.com/zsh-users/zsh-syntax-highlighting.git" "$plugins/zsh-syntax-highlighting"
-  "https://github.com/remcohaszing/zsh-node-bin.git" "$plugins/node-bin"
-  "https://github.com/fdellwing/zsh-bat.git" "$plugins/zsh-bat"
-  "https://github.com/lukechilds/zsh-nvm" "$plugins/zsh-nvm"
+custom="$HOME/.oh-my-zsh/custom/plugins"
+declare -A plugins=(
+  ["$custom/zsh-autosuggestions"]="https://github.com/zsh-users/zsh-autosuggestions.git"
+  ["$custom/zsh-syntax-highlighting"]="https://github.com/zsh-users/zsh-syntax-highlighting.git"
+  ["$custom/node-bin"]="https://github.com/remcohaszing/zsh-node-bin.git"
+  ["$custom/zsh-bat"]="https://github.com/fdellwing/zsh-bat.git"
+  ["$custom/zsh-nvm"]="https://github.com/lukechilds/zsh-nvm"
 )
 
-for repo dest in ${(kv)plugins}; do
-  [ -d "$dest/.git" ] || git clone "$repo" "$dest"
+
+for plugin in "${!plugins[@]}"; do
+  [ -d "$plugin/.git" ] || git clone "${plugins[$plugin]}" "$plugin"
 done
 
 # Install script for Catppuccin Tmux 
@@ -29,10 +32,12 @@ dir=$HOME/.config/tmux/plugins/catppuccin/tmux
 tpm=$HOME/.tmux/plugins/tpm
 [ -d "$tpm/.git" ] || git clone https://github.com/tmux-plugins/tpm "$tpm"
 
-tmx=$HOME/.timuxifire
+# Install tmuxifier
+tmx=$HOME/.tmuxifier
 [ -d "$tmx/.git" ] || git clone https://github.com/jimeh/tmuxifier.git "$tmx"
 
-# Apply simlinks 
+[ -d "$HOME/.zshrc" ] && mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+[ -d "$HOME/.config/nvim" ] &&  mv "$HOME/.config/nvim" "$HOME/.config/.nvim.backup"
 stow -t $HOME -d "$HOME/dotfiles" --ignore "README.md" --ignore "setup.sh" .
 
 # Install tmux plugins
