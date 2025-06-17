625426c67433404415b0f7e1ed8645e9de37b362
denchikut <ollylut@gmail.com>
2025-06-16 21:52:53 -0700

    telescope --glob search

--- .config/nvim/lua/configs/teleskope.lua
+++ .config/nvim/lua/configs/teleskope.lua
@@ -1,5 +1,14 @@
 local map = vim.keymap.set
+
 local builtin = require "telescope.builtin"
+local sorter = require "telescope.sorters"
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local actions = require "telescope.actions"
+local make_entry = require "telescope.make_entry"
+local action_state = require "telescope.actions.state"
+local live_grep = require("telescope.builtin").live_grep
+local conf = require("telescope.config").values
 
 map("n", "<leader>fc", builtin.commands, { desc = "Commands" })
 map("n", "<leader>fr", builtin.registers, { desc = "Registers" })
@@ -17,48 +26,58 @@ map("n", "<leader>fF", function()
       "--files",
       "--hidden",
       "--no-ignore",
-      "--glob",
-      "!**/.git/**",
-      "--glob",
-      "!**/.vercel/**",
-      "--glob",
-      "!**/.next/**",
-      "--glob",
-      "!**/dist/**",
-      "--glob",
-      "!**/node_modules/**",
+      "-g",
+      "!**/{.git,.vercel,.next,dist,node_modules}/**",
     },
   }
 end, { desc = "Find files (including hidden)" })
 
 map("n", "<leader>fW", function()
-  builtin.live_grep {
-    additional_args = function()
-      return {
-        "--hidden",
-        "--no-ignore",
-        "--glob",
-        "!**/.git/**",
-        "--glob",
-        "!**/.vercel/**",
-        "--glob",
-        "!**/.next/**",
-        "--glob",
-        "!**/dist/**",
-        "--glob",
-        "!**/node_modules/**",
-      }
-    end,
-  }
+  pickers
+    .new({}, {
+      debounce = 100,
+      prompt_title = "live grep (including hidden)",
+      finder = finders.new_async_job {
+        command_generator = function(prompt)
+          if not prompt or prompt == "" then
+            return nil
+          end
+
+          local pieces = vim.split(prompt, "  ")
+          local cmd = { "rg" }
+
+          if pieces[1] then
+            table.insert(cmd, "-e")
+            table.insert(cmd, pieces[1])
+          end
+
+          if pieces[2] then
+            table.insert(cmd, "-g")
+            table.insert(cmd, pieces[2])
+          end
+
+          return vim.tbl_flatten {
+            cmd,
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--no-ignore",
+            "-g",
+            "!**/{.git,.vercel,.next,dist,node_modules}/**",
+          }
+        end,
+        entry_maker = make_entry.gen_from_vimgrep {},
+        cwd = vim.uv.cwd(),
+      },
+      previewer = conf.grep_previewer {},
+      sorter = sorter.empty(),
+    })
+    :find()
 end, { desc = "telescope live grep (including hidden)" })
 
 map("n", "<leader>fd", function()
-  local pickers = require "telescope.pickers"
-  local finders = require "telescope.finders"
-  local conf = require("telescope.config").values
-  local actions = require "telescope.actions"
-  local action_state = require "telescope.actions.state"
-
   pickers
     .new({}, {
       prompt_title = "Select a directory",
@@ -78,16 +97,8 @@ map("n", "<leader>fd", function()
               "--files",
               "--hidden",
               "--no-ignore",
-              "--glob",
-              "!**/.git/**",
-              "--glob",
-              "!**/.vercel/**",
-              "--glob",
-              "!**/.next/**",
-              "--glob",
-              "!**/dist/**",
-              "--glob",
-              "!**/node_modules/**",
+              "-g",
+              "!**/{.git,.vercel,.next,dist,node_modules}/**",
             },
           }
         end)
@@ -98,12 +109,6 @@ map("n", "<leader>fd", function()
 end, { desc = "Find files (in the directory)" })
 
 map("n", "<leader>fg", function()
-  local pickers = require "telescope.pickers"
-  local finders = require "telescope.finders"
-  local conf = require("telescope.config").values
-  local actions = require "telescope.actions"
-  local action_state = require "telescope.actions.state"
-
   pickers
     .new({}, {
       prompt_title = "Select a directory for live grep",
@@ -114,23 +119,15 @@ map("n", "<leader>fg", function()
           local entry = action_state.get_selected_entry()
           actions.close(bufnr)
 
-          require("telescope.builtin").live_grep {
+          live_grep {
             cwd = entry[1],
             prompt_title = "Live Grep in " .. entry[1],
             additional_args = function()
               return {
                 "--hidden",
                 "--no-ignore",
-                "--glob",
-                "!**/.git/**",
-                "--glob",
-                "!**/.vercel/**",
-                "--glob",
-                "!**/.next/**",
-                "--glob",
-                "!**/dist/**",
-                "--glob",
-                "!**/node_modules/**",
+                "-g",
+                "!**/{.git,.vercel,.next,dist,node_modules}/**",
               }
             end,
           }
